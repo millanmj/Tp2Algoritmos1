d7be9a3e913485bcb171be0904b8c378dcb1b9b0
arielpasapera <arielpasapera@gmail.com>
2022-11-24 18:20:39 -0300

    actualización

--- speechRecog.py
+++ speechRecog.py
@@ -8,11 +8,18 @@ Dado que SpeechRecognition se envía con una clave de API predeterminada
 para Google Web Speech API, puede comenzar a usarla de inmediato. Por este 
 motivo, utilizaremos la Web Speech API en este tp.
 """
-#print(r.recognize_google())
-harvard = sr.AudioFile('.\\Audios_denuncias\\audio_files_harvard.wav')
-with harvard as source:
-  audio = r.record(source)
 
-#print(type(audio))
-print(r.recognize_google(audio))#imprime audio a texto
+def audio_a_texto (ruta_archivo:str):
+  prueba = sr.AudioFile(ruta_archivo)
+  with prueba as source:
+    audio = r.record(source)
+  denuncia = (r.recognize_google(audio,language='es-ES'))
+  return denuncia
 
+
+def main():
+
+ denuncia_1 = audio_a_texto(".\\Audios_denuncias\\denuncia1.wav")
+ print(denuncia_1)
+
+main()
